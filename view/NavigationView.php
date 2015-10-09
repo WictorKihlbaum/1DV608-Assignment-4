f8563a7d75f5b4a5bf2bbdd516521ab8316a6ecd
Wictor Kihlbaum <wk222as@student.lnu.se>
2015-10-09 07:57:47 +0000

    functions added to NavigationView

--- view/NavigationView.php
+++ view/NavigationView.php
@@ -3,4 +3,25 @@
 class NavigationView {
     
     private static $registerURL = "register";
+    
+    
+    public function getLinkToGoBack() {
+        
+		return "<a href='?'>Back to login</a>";
+	}
+	
+	public function getURLToRegisterForm() {
+	    
+		return "?" . self::$registerURL; // maybe have to change this.
+	}
+	
+	public function userWantsToRegister() {
+	    
+	    return isset($_GET[self::$registerURL]);
+	}
+	
+	public function getLinkToRegisterForm() {
+	    
+	    return '<a href="?' . self::$registerURL . '">Register a new user</a>';
+ 	}
 }
